3ee329da1b31cd7d17103ae546ef1c12532a24ed
tamgawa-ao <ti5p23336@stu.tamagawa.ac.jp>
2026-01-22 01:18:43 +0900

    second commit

--- SampleBBS/src/main/java/servlet/LoginServlet.java
+++ SampleBBS/src/main/java/servlet/LoginServlet.java
@@ -17,45 +17,25 @@ import dao.Dao;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    public LoginServlet() {
-        super();
-    }
-
-    //POSTアクセスの場合に動作
-    //GETアクセスは送信するパラメータがURLに載ってしまうので，データの登録や認証などには使用すべきでない．
-    //このサーブレットでは，idとパスワードを受け取ってログイン認証するので，POSTのみ受け付ける
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//requestから受け取る値の文字コードをUTF-8に設定．これがないと，たまに文字化けする．
 		request.setCharacterEncoding("UTF-8");
-		
-		//requestから，"id"というnameの値を取得
+
 		String id = request.getParameter("id");
-		//requestから，"password"というnameの値を取得
 		String password = request.getParameter("password");
 
-		//DBアクセスのためのクラスをインスタンス化
 		Dao dao = new Dao();
-		//idを渡し，そのidを持つユーザをDBから取得.Userはbeansパッケージに宣言してある．
 		User user = dao.getUserById(id);
 
-		//先ほどのidを持つユーザが存在し，かつパスワードが入力したものと一致していればArticleListServletにアクセスを移動．
-		if(user != null && user.getPassword().equals(password)) {
-			//Sessionオブジェクトを取得し，userIdという名前でidを格納．このセッションが生きている限り，いつでもuserIdを取り出せるようになる．
-			request.getSession().setAttribute("userId" , id);
-			RequestDispatcher dispatcher = request.getRequestDispatcher("./ArticleListServlet");
+		if (user != null && user.getPassword().equals(password)) {
+			request.getSession().setAttribute("userId", id);
+			response.sendRedirect(request.getContextPath() + "/ArticleListServlet");
+		} else {
+			request.setAttribute("loginError", "IDまたはパスワードが違います");
+			RequestDispatcher dispatcher =
+				request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
 			dispatcher.forward(request, response);
 		}
-		else {
-			//認証失敗の場合は，ログインページに戻す．
-			RequestDispatcher dispatcher = request.getRequestDispatcher("./WEB-INF/jsp/login.jsp");
-			dispatcher.forward(request, response);
-		}
-
 	}
-	
-	//POSTアクセスのみを受け付けたいので，doGetは定義しない．
-
-
 }
--- SampleBBS/src/main/webapp/WEB-INF/jsp/login.jsp
+++ SampleBBS/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -5,24 +5,40 @@
 <head>
 <meta charset="UTF-8">
 <title>Login page</title>
-<!-- スタイルシートの読み込み （src/main/webapp/css/style.css を参照）-->
-<link rel="stylesheet" type="text/css" href="./css/style.css" media="all" />
+<link rel="stylesheet" type="text/css"
+      href="<%= request.getContextPath() %>/css/style.css">
 </head>
 <body>
-<!--  LoginServletにデータを送信するformを宣言する. 登録処理なので，methodはpostを指定．
-	後はentryArticle.jspと同様なので，そちらのコメントを参考にするとよい．-->
-	<div id="form">
-	<p class="form-title" >Tiny BBS ログインページ</p>
-	<form action="./LoginServlet" method="post">
-		<p class="mail"><label>ログインID：<input type="text" name="id" size="30" maxlength="20"></label></p>
-		<p class="pass"><label>パスワード：<input type="password" name="password" size="20" maxlength="20"></label></p>
-		<p class="submit"><input type="submit" value="ログイン"></p>
-	</form>
-	<a href=./EntryUserPageServlet>ユーザ登録</a>
-	</div>
-		<!-- EntryUserPageServletにハイパーリンク．
-	ハイパーリンクによるアクセスは，GETアクセスとなり，サーブレットのdoGet()が呼ばれる． -->
-	
-	<br>
+
+<div id="form">
+  <p class="form-title">Tiny BBS ログインページ</p>
+
+  <% if (request.getAttribute("loginError") != null) { %>
+    <p style="color:red;">
+      <%= request.getAttribute("loginError") %>
+    </p>
+  <% } %>
+
+  <form action="<%= request.getContextPath() %>/LoginServlet" method="post">
+    <p class="mail">
+      <label>ログインID：
+        <input type="text" name="id" size="30" maxlength="20">
+      </label>
+    </p>
+    <p class="pass">
+      <label>パスワード：
+        <input type="password" name="password" size="20" maxlength="20">
+      </label>
+    </p>
+    <p class="submit">
+      <input type="submit" value="ログイン">
+    </p>
+  </form>
+
+  <a href="<%= request.getContextPath() %>/EntryUserPageServlet">
+    ユーザ登録
+  </a>
+</div>
+
 </body>
 </html>
